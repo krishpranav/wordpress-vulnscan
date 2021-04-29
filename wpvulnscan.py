89cd703e21067479a908b2cabe3c217e20dcb31b
krishpranav <krisna.pranav@gmail.com>
2021-04-29 11:02:47 +0530

    update wpvulnscan.py

---
+++ wpvulnscan.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env/python
+# -*- coding: utf-8 -*-
+
+# imports
+import subprocess
+
+# function for running wpscan
+def wpscan(url):
+	command = ['sudo', 'docker', 'run', '-it', '--rm', 'wpscanteam/wpscan', '-u', url, '--user-agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36', '--no-banner']
+	output = subprocess.run(command, stdout=subprocess.PIPE).stdout.decode('utf-8')
+	return output
+
+# main function
+def main():
+	url = input('[*] Enter the URL: ')
+	result = wpscan(url)
+	print()
+	print()
+	print(result)
+
+
+
+if __name__=='__main__':
+	main()
